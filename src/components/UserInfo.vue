93faf2c8bcbd94049c06a3c6690a3b17e4232710
Matheus Moreira <matheusm920@gmail.com>
2022-01-08 16:25:20 -0300

    initial sass in user view

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="user_info">Teste</div>
+</template>
+
+<style lang="scss">
+    .user_info {
+        display: flex;
+        width: 20%;
+        justify-items: start;
+        padding-left: 5%;
+        background-color: #fff;
+    }
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="user">
+        <TheHeader />
+        <UserInfo />
+        <TheFooter />
+    </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import TheFooter from '../components/TheFooter.vue';
+import UserInfo from '@/components/UserInfo.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+    UserInfo,
+  },
+};
+</script>
